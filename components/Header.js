6a50448f4162829e2f0242647bb425248623f1f3
GwangJin Byeon <qusrhkdwls45@gmail.com>
2022-08-23 23:07:07 +0900

    history

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import style from '../styles/header.module.css'
+
+export default function Header(){
+    return(
+        <header className={style.header}>
+            <div className={style.wrap_header}>
+                <div>
+
+                </div>
+            </div>
+        </header>
+    )
+}
---
+++ pages/posts/header_clone.js
@@ -0,0 +1,9 @@
+import Header from '../../components/Header'
+
+export default function Header_clone(){
+    return(
+        <Header>
+
+        </Header>
+    )
+}
