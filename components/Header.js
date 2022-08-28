aa733f392420ca06773b4d0f7b5381efce01e1e8
GwangJin Byeon <qusrhkdwls45@gmail.com>
2022-08-28 18:31:34 +0900

    반응형 헤더 기능 완료

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,69 @@
 import style from '../styles/header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
+import classNames from 'classnames'
+import { useState } from 'react'
 
 export default function Header(){
+
+    const [ headerMenuActive, setHeaderMenuActive ] = useState( false );
+    const [ headerIconsActive, setHeaderIconsActive ] = useState( false );
+    const headerMenuToggle = () => {
+        setHeaderMenuActive( (prev) => !prev );
+    };
+    const headerIconsToggle = () => {
+        setHeaderIconsActive( (prev) => !prev );
+    };
+
+    let complexCss = {
+        'header_logo': classNames({
+            [ style.header_logo ] : true,
+            [ style.header_item ] : true
+        }),
+        'header_menu': classNames({
+            [ style.header_menu ] : true,
+            [ style.header_item ] : true,
+            [ style.active ] : headerMenuActive
+        }),
+        'header_icons': classNames({
+            [ style.header_icons ] : true,
+            [ style.header_item ] : true,
+            [ style.active ] : headerIconsActive
+        }),
+        'header_bars': classNames({
+            [ style.header_bars ] : true,
+            [ style.header_item ] : true
+        }),
+    };
+
     return(
         <header className={ style.header }>
-            <div className={ style.header_logo }>
+            <div className={ complexCss.header_logo }>
                 <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+                Coding 블로그
             </div>
 
-            <ul className={ style.header_menu }>
+            <ul className={ complexCss.header_menu }>
                 <li>메뉴1</li>
                 <li>메뉴2</li>
                 <li>메뉴3</li>
             </ul>
 
-            <ul className={ style.header_icons }>
-                <li><FontAwesomeIcon icon={faTwitter} /></li>
-                <li><FontAwesomeIcon icon={faFacebook}/></li>
+            <ul className={ complexCss.header_icons }>
+                <li><FontAwesomeIcon icon={ faTwitter } /></li>
+                <li><FontAwesomeIcon icon={ faFacebook }/></li>
             </ul>
+
+            <div
+                className={ complexCss.header_bars }
+                onClick={() => {
+                    headerMenuToggle()
+                    headerIconsToggle()
+                }}
+            >
+                <FontAwesomeIcon icon={ faBars }></FontAwesomeIcon>
+            </div>
         </header>
     )
 }
