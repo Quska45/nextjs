805f9c0ddc97ef792de1eb072626722e1b609c0a
GwangJin Byeon <qusrhkdwls45@gmail.com>
2022-09-03 09:01:43 +0900

    header menu 테스트 코드 작성

--- test/Header/HeaderMenus.test.js
+++ test/Header/HeaderMenus.test.js
@@ -2,12 +2,13 @@
  * @jest-environment jsdom
  */
 
-import HeaderMenus from "../components/HeaderMenus";
+import HeaderMenus from "../../components/HeaderMenus";
 import { render } from "@testing-library/react"
-import { getMenus } from '../lib/menus'
+import { getMenus } from '../../lib/menus'
 
 it("matches snapshot", () => {
-    const menus = getMenus();
+    let menus = getMenus();
+
     const utils = render(<HeaderMenus menus={ menus }></HeaderMenus>)
     expect(utils.container).toMatchSnapshot()
 })
